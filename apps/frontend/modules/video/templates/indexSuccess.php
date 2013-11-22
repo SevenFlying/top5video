986e7482570b7f5e53312fa3e4a210a7d7e2433e
Seven Flying <diez.f.irene@gmail.com>
2013-11-22 20:22:49 +0100

    Muestra el top5 en la homepage

--- apps/frontend/modules/video/templates/indexSuccess.php
+++ apps/frontend/modules/video/templates/indexSuccess.php
@@ -1,32 +1,23 @@
 <h1>Videos List</h1>
 
-<table>
-  <thead>
-    <tr>
-      <th>Id</th>
-      <th>Author</th>
-      <th>Category</th>
-      <th>Title</th>
-      <th>Desc</th>
-      <th>Upload date</th>
-      <th>Url</th>
-      <th>Slug</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php foreach ($Videos as $Video): ?>
-    <tr>
-      <td><a href="<?php echo url_for('video/show?id='.$Video->getId()) ?>"><?php echo $Video->getId() ?></a></td>
-      <td><?php echo $Video->getAuthorId() ?></td>
-      <td><?php echo $Video->getCategoryId() ?></td>
-      <td><?php echo $Video->getTitle() ?></td>
-      <td><?php echo $Video->getDesc() ?></td>
-      <td><?php echo $Video->getUploadDate() ?></td>
-      <td><?php echo $Video->getUrl() ?></td>
-      <td><?php echo $Video->getSlug() ?></td>
-    </tr>
-    <?php endforeach; ?>
-  </tbody>
-</table>
+  <?php 
 
-  <a href="<?php echo url_for('video/new') ?>">New</a>
+    $c= new Criteria();
+    $c->setLimit(sfConfig::get('app_max_videos_on_homepage'));
+    $c->addJoin(VideoPeer::ID, ScorePeer::VIDEO_ID);
+    $c->addDescendingOrderByColumn(ScorePeer::STARS);
+    $c->addDescendingOrderByColumn(VideoPeer::UPLOAD_DATE);
+    $Videos= VideoPeer::doSelect($c)
+    
+    /*
+        select * from video where id in(
+        SELECT video_id,
+        FROM Score
+        GROUP BY video_id
+        ORDER BY AVG( stars ) DESC )
+    */
+?>
+ <?php foreach ($Videos as $Video): ?>
+ <h1><a href="<?php echo url_for('video/show?id='.$Video->getId()) ?>"><?php echo $Video->getTitle() ?></a></h1>
+  <div class="descr"><strong><?php echo $Video->getDesc() ?></strong> || Category: <?php echo $Video->getCategory() ?> || Upload date: <?php echo $Video->getUploadDate() ?></div>
+ <?php endforeach; ?>
